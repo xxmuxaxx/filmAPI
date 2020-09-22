f0df4f4f3895fb4813b57cda524841b98193ca55
xxmuxaxx <xxmuxaxx@mail.ru>
2020-09-22 19:04:25 +0300

    почистил проект, переписал компонент App на функциональный

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import { Route, Switch } from "react-router-dom";
 import FilmLib from "./containers/FilmLib/FilmLib";
 import FilmDetail from "./containers/FilmDetail/FilmDetail";
 
-class App extends Component {
-  render() {
-    return (
-      <Layout>
-        <Switch>
-          <Route path="/film/:title" component={FilmDetail} />
-          <Route path="/" component={FilmLib} />
-        </Switch>
-      </Layout>
-    );
-  }
+export default function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/film/:title" component={FilmDetail} />
+        <Route path="/" component={FilmLib} />
+      </Switch>
+    </Layout>
+  );
 }
-
-export default App;
