547db43741196970b3d065773ab59a5f89395e75
xxmuxaxx <xxmuxaxx@mail.ru>
2020-10-04 02:04:13 +0300

    Добавил футер

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 import Header from "../../components/Header/Header";
 import classes from "./Layout.module.css";
@@ -41,6 +42,22 @@ class Layout extends Component {
           value={this.state.searchInputValue}
         />
         <main>{this.props.children}</main>
+        <footer className="footer">
+          <div className="container">
+            <div className="footer__wrapper">
+              <NavLink to="/" className="logo">
+                <div className="logo__text-wrapper">
+                  <p className="logo__big-text">Кавказский</p>
+                  <p className="logo__text">кинопоиск</p>
+                </div>
+              </NavLink>
+              <div className="copyright">
+                <p className="copyright__top">Разработанно в студии</p>
+                <p className="copyright__bottom">Не твоих собачих дел</p>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
